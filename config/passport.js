d8178b2a42c9f8fff77595cb0aad44a7e3119e9a
parth <singlaparth15@gmail.com>
2026-03-29 00:15:38 +0530

    Fix the bugs of oauth

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:  '/auth/google/callback'
+    callbackURL:  'https://mentalist.codes/auth/google/callback'
   },
   (accessToken, refreshToken, profile, done) => {
     const user = {
